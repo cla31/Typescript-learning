4d90fd719d7975690c51f17ffb9fdebedba0ea06
Claire_M <clairerodriguez79@yahoo.fr>
2023-03-27 17:01:44 +0200

    declare variables with types

---
+++ types.js
@@ -0,0 +1,10 @@
+// var + nom de la variable: type de la variable = "contenu de la vaiable"
+// any signifie n'importe quel type.
+var testVariable;
+//Exemples:
+//Pour une string
+var lantern = 'Green Lantern';
+//Ou pour un number:
+var healthPoint = 100;
+//On peut déclarer nos propre type
+// let greenLantern: Heros = new Heros (nameLantern, healthPoint)
---
+++ types.ts
@@ -0,0 +1,26 @@
+// var + nom de la variable: type de la variable = "contenu de la vaiable"
+// any signifie n'importe quel type.
+var testVariable: any;
+
+//Exemples:
+//Pour une string
+var lantern: string ='Green Lantern';
+//Ou pour un number:
+var healthPoint: number = 100;
+
+
+class Heros {
+    nameLantern: string;
+    healthPoint: number;
+ 
+  
+    constructor(nameLantern: string, healthPoint: number) {
+      this.nameLantern = nameLantern;
+      this.healthPoint = healthPoint;
+    }
+  }
+//On peut déclarer nos propre type
+let greenLantern: Heros = new Heros (lantern, healthPoint)
+
+
+
