ec64ed46dcab965e2d2bfdc4ce0f345074b7969b
Claire_M <clairerodriguez79@yahoo.fr>
2023-03-27 21:52:27 +0200

    Classe Test in JS ES5

---
+++ classes.js
@@ -0,0 +1,8 @@
+//On obtient la classe Test mais en javascript ES5
+//Simulation d'une classe en tapant la commande:
+//tsc classes.ts --target ES5
+
+var Test = /** @class */ (function() {
+    function Test() {}
+    return Test;
+}());
